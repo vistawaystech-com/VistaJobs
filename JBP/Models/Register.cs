1f1167d45fc7d3b8aadbeecfd5b48fc9d9977e8e
Krishnaveni <Krishnaveni.v@vistawaystech.com>
2026-05-19 11:18:49 +0530

    Home page UI

--- JBP/Models/Register.cs
+++ JBP/Models/Register.cs
@@ -3,9 +3,9 @@
     public class Register
     {
         public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
 
     }
 }
