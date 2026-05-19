69abe06a6cb909588d9a3fcbb7a75bb291e065d8
LENOVO <LENOVO@DESKTOP-64VCOM8>
2026-05-19 16:23:27 +0530

    latest drop

---
+++ JBP/Migrations/20260519060903_FresherFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JBP.Migrations
+{
+    /// <inheritdoc />
+    public partial class FresherFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
