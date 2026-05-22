c6a5c1b6998fa7a77c021c623a083b27982d7ff6
Karthik <yourgmail@gmail.com>
2026-05-22 12:24:45 +0530

    latest

---
+++ JBP/Controllers/VerificationController.cs
@@ -0,0 +1,32 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace JBP.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class VerificationController : ControllerBase
+//    {
+//        private readonly IConfiguration _config;
+
+//        public VerificationController(IConfiguration config)
+//        {
+//            _config = config;
+//        }
+
+//        [HttpGet("digilocker-login")]
+//        public IActionResult DigiLockerLogin()
+//        {
+//            var clientId =
+//                _config["DigiLocker:ClientId"];
+
+//            var redirectUri =
+//                _config["DigiLocker:RedirectUri"];
+
+//            var url =
+//                $"https://api.digitallocker.gov.in/public/oauth2/1/authorize?response_type=code&client_id={clientId}&redirect_uri={redirectUri}";
+
+//            return Redirect(url);
+//        }
+//    }
+//}
