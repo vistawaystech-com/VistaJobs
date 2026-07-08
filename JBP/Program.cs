fd9751158107ef99ffffe996fc578cf69a629e35
AnanthaneniHari <harikrishna.a@vistawaystech.com>
2026-07-08 14:11:56 +0530

    azure changes

--- JBP/Program.cs
+++ JBP/Program.cs
@@ -105,15 +105,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Frontend runs from localhost during development, so API allows local browser calls.
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
+        options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500"
 
+                // Azure Frontend URL (deploy ayyaka replace cheyyi)
+                //"https://YOUR-FRONTEND.azurestaticapps.net"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 var app = builder.Build();
 
 app.UseSwagger();
@@ -136,7 +142,7 @@ app.UseStaticFiles(new StaticFileOptions
     RequestPath = "/Uploads"
 });
 
-app.UseCors("AllowAll");
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 
